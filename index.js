119872155fc20b773ee8fd7982b870c0bbd3ffc1
Akanle Jeremy <akanlejeremy@gmail.com>
2024-09-26 12:19:51 +0300

    filtering

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+const findMatching = (drivers, driver) => {
+    return drivers.filter(dri => dri.toLowerCase() == driver.toLowerCase())
+}
+
+console.log(findMatching(["John", "Peter", "James"], "peter"));
+
+const fuzzyMatch = (drivers, letters) => {
+    let letterLength = letters.length;
+    return drivers.filter((dri) => {
+        if (dri.slice(0, letterLength) == letters) {
+            return dri
+        }
+     }
+    )
+}
+
+const matchName = (drivers, driver) => {
+    return drivers.filter((dri) => {
+        for (let name in dri){
+            if (dri[name] === driver) {
+                return dri
+            }
+        }
+    })
+}
